14db6d09e815452df45a86849784be9bc7e7d7d8
kelvinokumu <oumakelvinokumu@gmail.com>
2026-01-20 10:41:40 +0300

    some changes

--- src/week1_2_intro/App.java
+++ src/week1_2_intro/App.java
@@ -5,5 +5,25 @@ public class App {
     public static void main(String[] args) throws Exception {
 
         System.out.println("Hello, World!");
+
+        String name  = "Sean";
+        int age, b,c ;
+        age = 25;
+        double price1 = 123.147;
+        
+        // display output
+        System.out.println("The name is "+name+ " the age is " +age);
+        
+        getResults();
+        App a1 = new App();
+        a1.getNames();
+    }
+
+    public static int getResults(){
+        return 0;
+    }
+
+    int getNames(){
+        return 0;
     }
 }
